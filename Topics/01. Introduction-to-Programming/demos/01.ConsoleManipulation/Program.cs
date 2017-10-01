a2bc46d183213c2698c7319e25b0ecd579d097a9
sorbg <seanoreillybg@gmail.com>
2017-10-01 13:57:43 +0300

    play with cursor position

--- Topics/01. Introduction-to-Programming/demos/01.ConsoleManipulation/Program.cs
+++ Topics/01. Introduction-to-Programming/demos/01.ConsoleManipulation/Program.cs
@@ -9,7 +9,9 @@ class Program
         Console.WindowHeight = 30;
         Console.WindowWidth = 100;
         Console.WriteLine("Hello");
+        Console.SetCursorPosition(15, 35);  // just a thought
         Console.WriteLine("Workshop!");
+        Console.SetCursorPosition(10, 35);  // makes the result appear before Workshop
         int result = 3 / 1;
         Console.WriteLine(result);
         //Console.Clear();
